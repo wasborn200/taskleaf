65f4f42ee96be78a5cd2ba0d3a91ac38d471021f
wasborn200 <archibald.ff1@gmail.com>
2019-09-11 07:45:57 +0900

    chapter4 p147

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_09_214050) do
+ActiveRecord::Schema.define(version: 2019_09_10_222041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_tasks_on_name", unique: true
   end
 
 end
